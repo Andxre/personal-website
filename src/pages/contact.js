ce6404092f5bc8599c3d5cc7a8e4be902068f98d
Andre Dasalla <apd6710@gmail.com>
2020-11-15 00:00:21 -0800

    Form Test

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,26 +1,91 @@
-import React from "react"
+import React, { useState } from "react"
+import { navigate } from "gatsby-link"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PageTransition from "gatsby-plugin-page-transitions"
 
-const ContactPage = () => (
-  <Layout>
-    <PageTransition>
-      <SEO title="Contact" />
-      <div className="wrapper">
-        <div className="project-title">
-          <h1>Contact Me</h1>
-          <p>Get in touch!</p>
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
+const ContactPage = () => {
+  const [state, setState] = useState({})
+
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
+
+  return (
+    <Layout>
+      <PageTransition>
+        <SEO title="Contact" />
+        <div className="wrapper">
+          <div className="project-title">
+            <h1>Contact Me</h1>
+            <p>Get in touch!</p>
+          </div>
+          <form
+            name="contact"
+            method="post"
+            action="/"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={handleSubmit}
+          >
+            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+            <input type="hidden" name="form-name" value="contact" />
+            <p hidden>
+              <label>
+                Don’t fill this out:{" "}
+                <input name="bot-field" onChange={handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your name:
+                <br />
+                <input type="text" name="name" onChange={handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Your email:
+                <br />
+                <input type="email" name="email" onChange={handleChange} />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message:
+                <br />
+                <textarea name="message" onChange={handleChange} />
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
         </div>
-        <div className="contact-info">
-          <h3>Email</h3>
-          <p>andredasalla@yahoo.com</p>
-          <h3>Discord</h3>
-          <p>Skwiid#2027</p>
-        </div>
-      </div>
-    </PageTransition>
-  </Layout>
-)
+      </PageTransition>
+    </Layout>
+  )
+}
 
 export default ContactPage
